f191faabd0b7f9a184d6c8bd9358ffc370582fa2
Nicole Turner <astartechie@gmail.com>
2025-04-18 11:04:33 +0100

    Added Logging

--- RayPlayingGame/Program.cs
+++ RayPlayingGame/Program.cs
@@ -1,8 +1,37 @@
-﻿namespace RayPlayingGame;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
-internal class Program
+namespace RayPlayingGame;
+
+internal class Program(ILogger<Program> logger)
 {
     private static void Main(string[] args)
     {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("log-.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddLogging(x => x.AddSerilog());
+        serviceCollection.AddSingleton<Program>();
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        var program = serviceProvider.GetService<Program>();
+
+        program?.Run();
+    }
+
+    public void Run()
+    {
+        try
+        {
+
+        }
+        catch (Exception exception)
+        {
+            logger.LogCritical(exception, exception.Message);
+        }
     }
 }
